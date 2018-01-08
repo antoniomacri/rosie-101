6a03054a807fc4417c62d3c3a7e73350f2378e01
Antonio Macrì <ing.antonio.macri@gmail.com>
2018-01-08 23:40:00 +0100

    Remove unused class.

--- rosie-101-web/src/main/java/com/github/antoniomacri/rosie/model/RosieConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.github.antoniomacri.rosie.model;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class RosieConfiguration {
-    public final String name = "Hello Rosie!";
-    public final Map<String, Object> map = new HashMap<>();
-}
